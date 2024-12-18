4bdd2f068a17c47ccf13aebc33c37d7aaafccfb8
Evan <evanyoussef8@gmail.com>
2024-12-17 23:24:52 -0500

    Added buoyant object script to be inhereted

--- boat-game/scripts/boat.gd
+++ boat-game/scripts/boat.gd
@@ -1,37 +1,8 @@
-extends RigidBody3D
-
-@export var buoyancy := 1.0
-@export var gravity := 10.0
-
-@export var drag := 0.05
-@export var angular_drag := 0.05
-
-@export var max_depth := 5.0
-
-var water_height := 0.0
-
-var submerged := false
+extends BuoyantObject
 
 func _physics_process(delta) -> void:
+	super._physics_process(delta)
 	handle_controls()
-	
-	for point in $BuoyancyPoints.get_children():
-		apply_buoyancy(delta, point.global_position)
-
-# Based on this video: https://www.youtube.com/watch?v=_R2KDcAp1YQ&t=227s
-func apply_buoyancy(delta, pos) -> void:
-	var depth = $"../Water/CloseWater".get_height(pos) - pos.y + 0.33
-	if depth > 0:
-		submerged = true
-		apply_force(Vector3.UP * buoyancy * gravity * clampf(depth, -1000, max_depth), pos - global_position)
-	else:
-		submerged = false
-
-# based on this video: https://www.youtube.com/watch?v=_R2KDcAp1YQ&t=227s
-func _integrate_forces(state) -> void:
-	if submerged:
-		state.linear_velocity *= 1 - drag
-		state.angular_velocity *= 1 - angular_drag
 
 func handle_controls():
 	$CameraSpring.global_position = global_position
